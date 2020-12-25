63aabd1ece852e06a085b1d6bd9e4deb2b6b11ca
Harvey <15339206608@163.com>
2020-12-26 00:24:36 +0800

    ✨ month

--- src/components/RemainingLife.ts
+++ src/components/RemainingLife.ts
@@ -1,5 +1,13 @@
 import { Options, Vue } from 'vue-class-component';
 
+function dateMonth(date1: Date, date2: Date): number {
+  const year1 = date1.getFullYear();
+  const month1 = date1.getMonth();
+  const year2 = date2.getFullYear();
+  const month2 = date2.getMonth();
+  return (year1 - year2) * 12 + month1 - month2;
+}
+
 @Options({
   props: {},
 })
@@ -7,8 +15,21 @@ export default class HelloWorld extends Vue {
   birth: Date = new Date();
 
   idealAge = 100;
+  unit = 3;
 
   get pastYear() {
     return new Date().getFullYear() - this.birth.getFullYear();
+  }
+
+  get pastMonth() {
+    return dateMonth(new Date(), this.birth);
+  }
+
+  get idealMonth() {
+    return this.idealAge * 12;
+  }
+
+  private disabledDate(date: Date) {
+    return date.getTime() > new Date().getTime();
   }
 }
--- src/components/RemainingLife.vue
+++ src/components/RemainingLife.vue
@@ -1,7 +1,12 @@
 <template>
   <el-form ref="form" label-width="80px">
     <el-form-item label="你的生日">
-      <el-date-picker v-model="birth" type="date" placeholder="Pick a day">
+      <el-date-picker
+        v-model="birth"
+        type="date"
+        placeholder="Pick a day"
+        :disabledDate="disabledDate"
+      >
       </el-date-picker>
     </el-form-item>
     <el-form-item label="理想岁数">
@@ -9,18 +14,37 @@
         v-model="idealAge"
         :min="pastYear"
         :max="120"
-        label="label"
       ></el-input-number>
+    </el-form-item>
+    <el-form-item label="单位">
+      <el-radio-group v-model="unit">
+        <el-radio :label="3">年</el-radio>
+        <el-radio :label="6">月</el-radio>
+      </el-radio-group>
     </el-form-item>
   </el-form>
 
-  <div class="age-area">
-    <div
-      class="year-item"
-      v-for="(item, index) in idealAge"
-      :key="index"
-      :class="{ past: item <= pastYear }"
-    ></div>
+  <div class="age-area" :class="{ year: unit === 3, month: unit === 6 }">
+    <template v-if="unit === 3 && pastYear > 0">
+      <div
+        class="item"
+        v-for="(item, index) in idealAge"
+        :key="index"
+        :class="{ past: item <= pastYear }"
+      >
+        <i v-if="item <= pastYear" class="el-icon-close"></i>
+      </div>
+    </template>
+    <template v-else-if="unit === 6 && pastMonth > 0">
+      <div
+        class="item"
+        v-for="(item, index) in idealMonth"
+        :key="index"
+        :class="{ past: item <= pastMonth }"
+      >
+        <i v-if="item <= pastMonth" class="el-icon-close"></i>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -28,20 +52,33 @@
 
 <style lang="scss" scoped>
 .age-area {
-  display: grid;
-  grid-template-columns: repeat(10, 1fr);
-  gap: 10px;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10px;
-  justify-items: center;
-  .year-item {
-    border: 1px solid #000;
-    box-sizing: border-box;
-    height: 40px;
-    width: 40px;
-    &.past {
-      background-color: #ccc;
+  display: flex;
+  flex-wrap: wrap;
+  &.year {
+    grid-template-columns: repeat(10, 1fr);
+    .item {
+      border: 1px solid #000;
+      box-sizing: border-box;
+      height: 50px;
+      width: 50px;
+      text-align: center;
+      line-height: 50px;
+      font-size: 50px;
+      &.past {
+        background-color: #ccc;
+      }
+    }
+  }
+  &.month {
+    grid-template-columns: repeat(50, 1fr);
+    .item {
+      border: 1px solid #000;
+      box-sizing: border-box;
+      height: 20px;
+      width: 20px;
+      &.past {
+        background-color: #ccc;
+      }
     }
   }
 }
